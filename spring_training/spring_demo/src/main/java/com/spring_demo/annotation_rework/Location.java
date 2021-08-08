082ef7c0fdd75f62f4a4618b543589953ec0df86
Sethu Naga Karthik M <m.sethukarthik@gmail.com>
2021-08-08 23:30:49 +0530

    Annotation Rework

--- spring_training/spring_demo/src/main/java/com/spring_demo/annotation_rework/Location.java
+++ spring_training/spring_demo/src/main/java/com/spring_demo/annotation_rework/Location.java
@@ -1,12 +1,15 @@
 package com.spring_demo.annotation_rework;
 
+import javax.inject.Inject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Location {
 	
-	@Autowired
+	@Inject
+//	@Autowired -> Both work as similar the only difference @Autowired is from Spring and @Inject is from JEE or JSR 330
 	private MedicalShop medicalShop;
 	
 	private String loco;
