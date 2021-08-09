0ad55a6a7e00725e697c67aa9c0515ea22ac385d
Sethu Naga Karthik M <m.sethukarthik@gmail.com>
2021-08-09 15:43:59 +0530

    Using Id and Name

--- spring_training/spring_demo/src/main/java/com/spring_demo/maven_demo/ExcuteMavenDemo.java
+++ spring_training/spring_demo/src/main/java/com/spring_demo/maven_demo/ExcuteMavenDemo.java
@@ -9,12 +9,15 @@ public class ExcuteMavenDemo {
 	public static void main(String[] args) {
 		
 		ClassPathXmlApplicationContext cpxac = new ClassPathXmlApplicationContext("maven_demo.xml");
-		Account acc = cpxac.getBean("account_1", Account.class);
-		System.out.println(acc);
-		acc.print();
-		Account acc_1 = cpxac.getBean("account_1", Account.class);
-		System.out.println(acc_1);
-		Train t = (Train) cpxac.getBean("javaT");
+		Account acc = (Account) cpxac.getBean("acc_1");
+		acc.setCustomer(new Customer(1,"A"));
+		System.out.println(acc.getCustomer());
+//		Account acc = cpxac.getBean("account_1", Account.class);
+//		System.out.println(acc);
+//		acc.print();
+//		Account acc_1 = cpxac.getBean("account_1", Account.class);
+//		System.out.println(acc_1);
+//		Train t = (Train) cpxac.getBean("javaT");
 		//Singleton will available until the container get close(It will cache by the container)
 		//Prototype will available until his job done. Once his job done it will garbage collected (It won't cache by the container)
 		cpxac.close();
